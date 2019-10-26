8cae96f283549f7e24da4475b26fb2c11a21a309
kalogirou <vaskalogirou@gmail.com>
2019-10-26 15:05:54 +0300

    first commit

---
+++ src/main/java/com/kalogirou/anixe/AnixeAssignmentKalogirouApplication.java
@@ -0,0 +1,11 @@
+package com.kalogirou.anixe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnixeAssignmentKalogirouApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AnixeAssignmentKalogirouApplication.class, args);
+	}
+}
