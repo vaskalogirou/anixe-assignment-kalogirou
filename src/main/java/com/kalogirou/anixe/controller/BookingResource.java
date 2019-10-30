d11db0d889b805eea91ffb7d8fb9467b5a136aa5
kalogirou <vaskalogirou@gmail.com>
2019-10-30 21:28:40 +0200

    add createBooking endpoint and some tests

--- src/main/java/com/kalogirou/anixe/controller/BookingResource.java
+++ src/main/java/com/kalogirou/anixe/controller/BookingResource.java
@@ -2,7 +2,13 @@ package com.kalogirou.anixe.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +22,15 @@ public class BookingResource {
 
 	public BookingResource(BookingRepository bookingRepository) {
 		this.bookingRepository = bookingRepository;
+	}
+
+	@PostMapping("/bookings")
+	public ResponseEntity<?> createBooking(@Valid @RequestBody Booking booking) {
+		if (booking.getId() != null) {
+			return ResponseEntity.badRequest().body("A new booking cannot already have an id");
+		}
+		bookingRepository.save(booking);
+		return ResponseEntity.status(HttpStatus.CREATED).body(booking);
 	}
 
 	@GetMapping("/bookings")
--- src/test/java/com/kalogirou/anixe/controller/BookingResourceIntTest.java
+++ src/test/java/com/kalogirou/anixe/controller/BookingResourceIntTest.java
@@ -1,18 +1,27 @@
 package com.kalogirou.anixe.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kalogirou.anixe.AnixeAssignmentKalogirouApplication;
+import com.kalogirou.anixe.domain.Booking;
+import com.kalogirou.anixe.domain.Hotel;
+import com.kalogirou.anixe.helper.Currency;
+import com.kalogirou.anixe.helper.CurrencyUtils;
 import com.kalogirou.anixe.repository.BookingRepository;
+import com.kalogirou.anixe.repository.HotelRepository;
 
 @SpringBootTest(classes = AnixeAssignmentKalogirouApplication.class)
 public class BookingResourceIntTest {
@@ -21,10 +30,58 @@ public class BookingResourceIntTest {
 	@Autowired
 	private BookingRepository bookingRepository;
 
+	@Autowired
+	private HotelRepository hotelRepository;
+
+	private Booking booking;
+
 	@BeforeEach
 	public void setup() {
+		Hotel hotel = new Hotel();
+		hotel.setName("dummy hotel");
+		hotel.setAddress("dummy address");
+		hotel.setStarRating(7.5f);
+		hotelRepository.save(hotel);
+
+		booking = new Booking();
+		booking.setCustomerName("john");
+		booking.setCustomerSurname("doe");
+		booking.setCurrency(Currency.EUR);
+		booking.setNumberOfPax(2);
+		booking.setPriceAmount(100f);
+		booking.setCurrentRateToEuro(CurrencyUtils.getRates().get(Currency.EUR));
+		booking.setHotel(hotel);
+
 		final BookingResource bookingResource = new BookingResource(bookingRepository);
 		mockMvc = MockMvcBuilders.standaloneSetup(bookingResource).build();
+	}
+
+	@Test
+	@Transactional
+	public void createBooking() throws Exception {
+		int databaseSizeBeforeCreation = bookingRepository.findAll().size();
+		mockMvc.perform(post("/api/bookings")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(TestUtil.convertObjectToJsonBytes(booking)))
+				.andExpect(status().isCreated());
+
+		int databaseSizeAfterCreation = bookingRepository.findAll().size();
+		assertThat(databaseSizeAfterCreation).isEqualTo(databaseSizeBeforeCreation + 1);
+	}
+
+	@Test
+	@Transactional
+	public void createBookingWithExistingId() throws Exception {
+		booking.setId(1L);
+		int databaseSizeBeforeCreation = bookingRepository.findAll().size();
+		mockMvc.perform(post("/api/bookings")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(TestUtil.convertObjectToJsonBytes(booking)))
+				.andExpect(status().isBadRequest())
+				.andExpect(content().string("A new booking cannot already have an id"));
+
+		int databaseSizeAfterCreation = bookingRepository.findAll().size();
+		assertThat(databaseSizeAfterCreation).isEqualTo(databaseSizeBeforeCreation);
 	}
 
 	@Test
