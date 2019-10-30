453d3ee4c809e432219df3d8ea4d4c561a264d85
kalogirou <vaskalogirou@gmail.com>
2019-10-30 15:06:15 +0200

    add HotelRepositoryIT class, in order to test the schema constraints of the relevant database table

---
+++ src/test/java/com/kalogirou/anixe/repository/HotelRepositoryIT.java
@@ -0,0 +1,43 @@
+package com.kalogirou.anixe.repository;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kalogirou.anixe.AnixeAssignmentKalogirouApplication;
+import com.kalogirou.anixe.domain.Hotel;
+
+@SpringBootTest(classes = AnixeAssignmentKalogirouApplication.class)
+public class HotelRepositoryIT {
+	@Autowired
+	private HotelRepository hotelRepository;
+
+	private Hotel hotel;
+
+	@BeforeEach
+	public void setup() {
+		hotel = new Hotel();
+		hotel.setName("dummy name");
+		hotel.setAddress("dummy address");
+	}
+
+	@Test
+	@Transactional
+	public void saveHotelWithoutName() {
+		hotel.setName(null);
+		assertThrows(ConstraintViolationException.class, () -> hotelRepository.saveAndFlush(hotel));
+	}
+
+	@Test
+	@Transactional
+	public void saveHotelWithoutAddress() {
+		hotel.setAddress(null);
+		assertThrows(ConstraintViolationException.class, () -> hotelRepository.saveAndFlush(hotel));
+	}
+}
